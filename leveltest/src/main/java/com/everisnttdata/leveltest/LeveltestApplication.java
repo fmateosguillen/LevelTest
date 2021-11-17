547bc880957aa8959b71cabef2d3eaed2d4d2c62
USERSAD\fmateosg <fmateosg@everis.com>
2021-11-17 21:34:53 +0100

    First Commit: Project structure created and solved a warning about JDK

---
+++ leveltest/src/main/java/com/everisnttdata/leveltest/LeveltestApplication.java
@@ -0,0 +1,13 @@
+package com.everisnttdata.leveltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeveltestApplication.class, args);
+	}
+
+}
---
+++ leveltest/src/test/java/com/everisnttdata/leveltest/LeveltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisnttdata.leveltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeveltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
