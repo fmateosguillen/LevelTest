dc72f6e5e0f753f2b797ed82e5b3b606f3e75a51
USERSAD\fmateosg <fmateosg@everis.com>
2021-11-18 17:21:11 +0100

    Small modifications. Added more html files for navigation porpouses. I still can't extract information form the excel and because of that, the controllers doesn't work as they should

--- leveltest/src/main/java/com/everisnttdata/leveltest/controllers/CountryController.java
+++ leveltest/src/main/java/com/everisnttdata/leveltest/controllers/CountryController.java
@@ -4,20 +4,36 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everisnttdata.leveltest.models.Country;
 
 @RestController
+@RequestMapping("/")
 public class CountryController {
-
-	@GetMapping("/countries")
-	public String countries() {
+	
+	//menú principal
+	@GetMapping("/index")
+	public String goToIndex() {
+		return "index";
+	}
+	
+	//carga el listado de paises
+	@GetMapping("/get-countries")
+	public String getCountries() {
 		return "countries";
 	}
 	
+	//se redirige la página donde se carga el excel
+	@GetMapping("/load-excel")
+	public String LoadExcel() {
+		return "submit-excel";
+	}
+	
+	//se carga el excel y se redirige al menú principal
 	@PostMapping("/submit-excel")
-	public String submitex() {
-		return "subex";
+	public String SubmitExcel() {
+		return "redirect:/index";
 	}
 }
